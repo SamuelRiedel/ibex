f01b950dad38dc9ba13d492be5d93dab2e645549
Samuel Riedel <sriedel@lowrisc.org>
2025-11-07 11:00:17 +0000

    [rvfi] Track the final expanded instruction in rvfi

--- rtl/ibex_top_tracing.sv
+++ rtl/ibex_top_tracing.sv
@@ -141,6 +141,7 @@ module ibex_top_tracing import ibex_pkg::*; #(
   logic        rvfi_ext_ic_scr_key_valid;
   logic        rvfi_ext_irq_valid;
   logic [15:0] rvfi_ext_expanded_insn;
+  logic        rvfi_ext_expanded_insn_last;
 
   logic [31:0] unused_perf_regs [10];
   logic [31:0] unused_perf_regsh [10];
@@ -157,6 +158,7 @@ module ibex_top_tracing import ibex_pkg::*; #(
   logic        unused_rvfi_ext_ic_scr_key_valid;
   logic        unused_rvfi_ext_irq_valid;
   logic [15:0] unused_rvfi_ext_expanded_insn;
+  logic        unused_rvfi_ext_expanded_insn_last;
 
   // Tracer doesn't use these signals, though other modules may probe down into tracer to observe
   // them.
@@ -173,6 +175,7 @@ module ibex_top_tracing import ibex_pkg::*; #(
   assign unused_rvfi_ext_ic_scr_key_valid = rvfi_ext_ic_scr_key_valid;
   assign unused_rvfi_ext_irq_valid = rvfi_ext_irq_valid;
   assign unused_rvfi_ext_expanded_insn = rvfi_ext_expanded_insn;
+  assign unused_rvfi_ext_expanded_insn_last = rvfi_ext_expanded_insn_last;
 
   ibex_top #(
     .PMPEnable        ( PMPEnable        ),
@@ -285,6 +288,7 @@ module ibex_top_tracing import ibex_pkg::*; #(
     .rvfi_ext_ic_scr_key_valid,
     .rvfi_ext_irq_valid,
     .rvfi_ext_expanded_insn,
+    .rvfi_ext_expanded_insn_last,
 
     .fetch_enable_i,
     .alert_minor_o,
